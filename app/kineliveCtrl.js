aa9b419336e759a33fa7d57ecf3155e6bea615e6
Rodrigo <reyes.rr@gmail.com>
2016-03-05 21:49:48 +0100

    better swipes

--- app/kineliveCtrl.js
+++ app/kineliveCtrl.js
@@ -4,17 +4,19 @@ var patients = [
     {
         firstName: "Jean",
         lastName: "Dupond",
-        nextMeeting: new Date(),
+        nextMeeting: new Date(2016, 3, 1, 10, 00),
         age: 43,
+        address: "8 rue Daguerre, 75014 Paris",
         pathology: "Parkinson",
         profilImage: "img/profils/generic-h.png"
     },
     {
         firstName: "Irène",
         lastName: "Larrivé",
-        nextMeeting: new Date(),
+        nextMeeting: new Date(2016, 3, 1, 10, 30),
         age: 88,
         pathology: "Parkinson",
+        address: "342 av du Maine, 75014 Paris",
         profilImage: "img/profils/generic-f.png"
     }
 
@@ -23,13 +25,65 @@ var patients = [
 kineApp.controller('KineliveController', function ($scope) {
     $scope.page = "home";
     $scope.currentPatient = patients[0];
+    $scope.pageTitle = "";
 
     $scope.setPage = function(pageName) {
         $scope.page = pageName;
-    }
+        console.log("set page:", pageName);
+        switch (pageName) {
+            case "patients":
+                $scope.setPageTitle("Vos patients");
+                break;
+            case "profil":
+                $scope.setPageTitle("Dossier patient");
+                break;
+            case "outils":
+                $scope.setPageTitle("Vos outils");
+                break;
+            case "symptomes":
+                $scope.setPageTitle("Symptômes");
+                break;
+            case "traitements":
+                $scope.setPageTitle("Exercices");
+                break;
+
+            default:
+                $scope.setPageTitle("");
+        }
+    };
+
+    $scope.swipeBack= function() {
+        console.log("swipe back");
+        switch ($scope.page) {
+            case "patients":
+                $scope.setPage("home");
+                break;
+            case "profil":
+                $scope.setPage("patients");
+                break;
+            case "outils":
+                $scope.setPage("home");
+                break;
+            case "symptomes":
+                $scope.setPage("outils");
+                break;
+            case "traitements":
+                $scope.setPage("symptomes");
+                break;
+
+            default:
+                $scope.setPage("home");
+                break;
+        }
+    };
+
 
     $scope.setPatient = function(patient) {
         $scope.currentPatient = patient;
+    }
+
+    $scope.setPageTitle = function(title) {
+        $scope.pageTitle = title;
     }
 
     $scope.patientService = new PatientService();
